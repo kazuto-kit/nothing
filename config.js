0d253e1a7d5ec9747b1ce5b4a8331ba2ef5e49ff
kazuto-kit <nguyendinhphu9.10@gmail.com>
2024-11-16 16:57:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Anos",
+    introTitle: 'Cục Cưng bé dàng Lê Bảo Hân',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnIntro: '^^EHeHeHe^^',
+    title: 'Anh xin lỗi cục cưng của anh nhiều lắm vì anh đã phạm tội tày đình😥😔😔😔',
+    desc: 'Em tha lỗi cho anh nha anh chừa rồi thiếu em anh sợ lắm😭😭🥺🥺🥺😭 ',
+    btnYes: 'Dạ nốt lần này thôi nhé<3',
+    btnNo: 'Không bao giờ >:(',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bé lại yêu anh <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Tại anh ngốc biết điều đáng iuu<33333333',
+    mess: 'Anh biết mò 🥰(´▽`ʃ♡ƪ). Yêu bé nhiều nhiều lém lun😘😘o((>ω< ))o',
+    messDesc: 'Trưa thứ 3 anh bù cho bé nho 1 matcha latte nhé.',
+    btnAccept: 'Dạ okiiiii lun <3🥰',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
